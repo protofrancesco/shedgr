424e75f2af0f7dcb27dbf4ac7e48349c37c45cde
Francesco Proto <protofra@gmail.com>
2021-10-11 16:35:25 +0200

    gtag

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import "@/assets/code-highlight.css"
 // in your main.js or similar file
 import Vue from 'vue'
 import cookieconsent from 'vue-cookieconsent-component'
+import VueGtag from 'vue-gtag'
 
+Vue.use(VueGtag, {  config: { id: "UA-134417952-5" }});
 
 Vue.component('cookie-consent', cookieconsent)
 
